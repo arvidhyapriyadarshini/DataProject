a9be7ac83c366d8159a3f1fbe775eefd9324fdc8
arvidhyapriyadarshini <112971590+arvidhyapriyadarshini@users.noreply.github.com>
2022-09-06 14:31:20 -0400

    Program reads latest value in text file
    This program reads the latest updated value in a text file.

---
+++ readLatestTextfile.py
@@ -0,0 +1,9 @@
+def readLatestEntry():
+    with open('C:\\DataPythonProject\\randomTempDict3.txt', 'r') as file:
+        for last_line in file:
+            pass
+
+    return last_line
+
+
+print(readLatestEntry())
